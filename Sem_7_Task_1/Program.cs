33e91a1265ba014091ebff95ab48515cebbcf075
DanilaVetyugov <danila_062014@mail.ru>
2024-06-22 02:56:55 +0300

    Задание 1 выполнил

---
+++ Sem_7_Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение M:");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Все натуральные числа в промежутке от {M} до {N}:");
+        pNum(M, N);
+    }
+
+    static void pNum(int M, int N)
+    {
+        if (M > N)
+        {
+            return;
+        }
+
+        Console.Write(M + " ");
+        pNum(M + 1, N);
+    }
+}
