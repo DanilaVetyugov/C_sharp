3b2f77bf3686872d9cebfd2e581ccc2d54bdb2cd
DanilaVetyugov <danila_062014@mail.ru>
2024-06-22 02:58:52 +0300

    ex_3 Dun

---
+++ Sem_7_Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintArrayFromEnd(int[] arr, int index)
+{
+    if (index < 0) return;
+    Console.Write($"{arr[index]} ");
+    PrintArrayFromEnd(arr, index - 1);
+}
+
+Console.WriteLine("Введите количество элементов в массиве:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+Console.WriteLine("Введите элементы массива:");
+for (int i = 0; i < size; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+PrintArrayFromEnd(array, size - 1);
